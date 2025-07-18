1528cd5eca0546b6647e86208adcfd37b925e078
Maha6468 <motherma7633@gmail.com>
2025-07-19 00:38:32 +0600

    Buttom TabBar,label Colors

--- lib/widget/appBar.dart
+++ lib/widget/appBar.dart
@@ -47,11 +47,13 @@ class _AppBar_TState extends State<AppBar_T> with SingleTickerProviderStateMixin
         actions: [
           Icon(Icons.favorite),
           IconButton(onPressed: (){
-            print('this is iconButton');
+            print('This is iconButton');
           },icon:Icon(Icons.search))
         ],
         bottom: TabBar(
           controller: _tabController,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.deepOrange,
           tabs: [
             Tab(icon: Icon(Icons.person),
             text: 'Person',),
@@ -62,6 +64,25 @@ class _AppBar_TState extends State<AppBar_T> with SingleTickerProviderStateMixin
           ],
         ),
         ),
+      bottomNavigationBar: Material(
+        color: Colors.green,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.only(topRight: Radius.circular(20),topLeft: Radius.circular(20))
+        ),
+        child: TabBar(
+          controller: _tabController,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.deepOrange,
+          tabs: [
+            Tab(icon: Icon(Icons.person),
+              text: 'Person',),
+            Tab(icon: Icon(Icons.card_travel),
+              text: 'Travel',),
+            Tab(icon: Icon(Icons.shopping_cart),
+              text: 'Shopping',)
+          ],
+        ),
+      ),
     );
   }
 }
